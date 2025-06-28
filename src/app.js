9a41f01fbeb0d00554581c30a85d63285cbdc1d4
LauraCwell <lauracardwell45@gmail.com>
2025-06-28 17:36:34 +0100

    injected forecast HTML through JS

--- src/app.js
+++ src/app.js
@@ -63,5 +63,31 @@ function handleSearch(event) {
 let cityFormElement = document.querySelector("#city-form");
 cityFormElement.addEventListener("submit", handleSearch);
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 updateDateTime();
 searchCity("London");
+displayForecast();
